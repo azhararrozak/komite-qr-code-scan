8134b952acfe4e3d2c347fcf07b69ce61f4e6cf7
Muhammad Affan Tamami <80788855+muhammadaffant@users.noreply.github.com>
2025-10-15 08:12:40 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // --- Menjalankan Server ---
 // PERBAIKAN: Hanya ada satu deklarasi PORT
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
